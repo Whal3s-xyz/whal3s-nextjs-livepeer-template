b9bf7f34c1c0c3d28de95bfdcf21f1711a6335a0
Jonas Esser <mail@jonas-esser.de>
2023-04-04 16:29:16 +0200

    Small bug fix (imports)

--- src/pages/gated/video.tsx
+++ src/pages/gated/video.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Layout from "@/components/Layout";
-import {withIronSessionSsr} from "iron-session/next/index";
+import {withIronSessionSsr} from "iron-session/next";
 import {sessionOptions} from "@/lib/session";
 
 // @ts-ignore
 const Video = ({user}) => {
     return (
       <Layout header="Gated video">
-          <p>https://playback.livepeer.studio/asset/hls/f9f96eryysmjdbnk/index.m3u8?{user.accessToken}</p>
-          <video src={`https://playback.livepeer.studio/asset/hls/f9f96eryysmjdbnk/index.m3u8?${user.accessToken}`} controls className="w-full"/>
+          <p>https://playback.livepeer.studio/asset/hls/f9f96eryysmjdbnk/index.m3u8?accessKey={user.accessToken}</p>
+          <video src={`https://playback.livepeer.studio/asset/hls/f9f96eryysmjdbnk/index.m3u8?accessKey==${user.accessToken}`} controls className="w-full"/>
       </Layout>
     );
 };
