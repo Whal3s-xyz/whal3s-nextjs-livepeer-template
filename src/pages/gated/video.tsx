0d4da620057b67d4c7e0390a0f79e37dd55b0eea
Jonas Esser <mail@jonas-esser.de>
2023-04-04 17:29:42 +0200

    FIX: Play video with react-player

--- src/pages/gated/video.tsx
+++ src/pages/gated/video.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Layout from "@/components/Layout";
 import {withIronSessionSsr} from "iron-session/next";
 import {sessionOptions} from "@/lib/session";
+import ReactPlayer from 'react-player'
 
 // @ts-ignore
 const Video = ({user}) => {
     return (
       <Layout header="Gated video">
           <p>https://playback.livepeer.studio/asset/hls/f9f96eryysmjdbnk/index.m3u8?accessKey={user.accessToken}</p>
-          <video src={`https://playback.livepeer.studio/asset/hls/f9f96eryysmjdbnk/index.m3u8?accessKey=${user.accessToken}`} controls className="w-full"/>
+          <ReactPlayer url={`https://playback.livepeer.studio/asset/hls/f9f96eryysmjdbnk/index.m3u8?accessKey=${user.accessToken}`}  controls={true} />
       </Layout>
     );
 };
