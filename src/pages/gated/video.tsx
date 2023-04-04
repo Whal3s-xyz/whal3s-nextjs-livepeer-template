d70cf128e82b42f09578340bff604511347f74e2
Jonas Esser <mail@jonas-esser.de>
2023-04-04 16:22:26 +0200

    Added livepeer video to gated page

--- src/pages/gated/video.tsx
+++ src/pages/gated/video.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import Layout from "@/components/Layout";
+import {withIronSessionSsr} from "iron-session/next/index";
+import {sessionOptions} from "@/lib/session";
 
-const Video = () => {
+// @ts-ignore
+const Video = ({user}) => {
     return (
       <Layout header="Gated video">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/uIhmVQTmUzs?controls=0"
-                  title="YouTube video player" frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  allowFullScreen></iframe>
+          <p>https://playback.livepeer.studio/asset/hls/f9f96eryysmjdbnk/index.m3u8?{user.accessToken}</p>
+          <video src={`https://playback.livepeer.studio/asset/hls/f9f96eryysmjdbnk/index.m3u8?${user.accessToken}`} controls className="w-full"/>
       </Layout>
     );
 };
+
+export const getServerSideProps = withIronSessionSsr(
+    async function getServerSideProps({ req }) {
+
+        return {
+            props: {
+                user: req?.session?.user ?? null,
+            },
+        };
+    },
+    sessionOptions
+);
 
 export default Video;
