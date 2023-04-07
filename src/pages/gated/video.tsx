b49373bfafcac21c7acaee395dd18e5bc86c98ad
Jonas Esser <mail@jonas-esser.de>
2023-04-07 23:18:35 +0200

    Make use of Livepeer player

--- src/pages/gated/video.tsx
+++ src/pages/gated/video.tsx
@@ -2,20 +2,28 @@ import React from 'react';
 import Layout from "@/components/Layout";
 import {withIronSessionSsr} from "iron-session/next";
 import {sessionOptions} from "@/lib/session";
-// import ReactPlayer from 'react-player'
 import {User} from "@/pages/api/user";
 import dynamic from "next/dynamic";
+import {createReactClient, LivepeerConfig, studioProvider} from "@livepeer/react";
 
-const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
+const Player = dynamic(() => import('@livepeer/react').then((mod) => mod.Player), { ssr: false });
 
 type Props = {
     user?: User
 }
+
+const livepeerClient = createReactClient({
+    provider: studioProvider({
+        apiKey: process.env.NEXT_PUBLIC_STUDIO_API_KEY ?? '',
+    }),
+});
 const Video = ({user}: Props) => {
     return (
       <Layout header="Gated video">
+          <LivepeerConfig client={livepeerClient}>
+            <Player playbackId='f9f96eryysmjdbnk' accessKey={user?.accessToken} />
+          </LivepeerConfig>
           {/*<p>https://playback.livepeer.studio/asset/hls/f9f96eryysmjdbnk/index.m3u8?accessKey={user?.accessToken}</p>*/}
-          <ReactPlayer url={`https://playback.livepeer.studio/asset/hls/f9f96eryysmjdbnk/index.m3u8?accessKey=${user?.accessToken}`}  controls={true} />
       </Layout>
     );
 };
